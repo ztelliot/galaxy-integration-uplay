ae8272cecb25c47aa7fb247a75e6d87bae755c34
FriendsOfGalaxy <friendsofgalaxy@gmail.com>
2020-09-21 11:05:45 +0200

    version 0.55.2

--- src/version.py
+++ src/version.py
@@ -1,6 +1,9 @@
-__version__ = '0.55.1'
+__version__ = '0.55.2'
 
 __changelog__ = {
+    "0.55.2": """
+        - bump galaxy.plugin.api version for more stable `get_local_size`
+    """,
     "0.55.1": """
         - fix potential crashes due to blocking `get_local_size` method
     """
